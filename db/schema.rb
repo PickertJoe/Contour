9b6a3bf13e53ce3f7c5987d968f45ece0757e359
dilbert250 <pickertjoe@gmail.com>
2019-10-28 11:31:18 -0500

    Creating migrations for Topo and Elevation fields; migrating db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_28_162704) do
+ActiveRecord::Schema.define(version: 2019_10_28_162928) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -36,6 +36,11 @@ ActiveRecord::Schema.define(version: 2019_10_28_162704) do
   create_table "elevations", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "gpx_id"
+    t.string "chart_title"
+    t.string "x_title"
+    t.string "y_title"
+    t.integer "size"
   end
 
   create_table "gpxes", force: :cascade do |t|
@@ -48,6 +53,9 @@ ActiveRecord::Schema.define(version: 2019_10_28_162704) do
   create_table "topos", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "gpx_id"
+    t.string "chart_title"
+    t.integer "size"
   end
 
   create_table "users", force: :cascade do |t|
