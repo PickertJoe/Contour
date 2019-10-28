087524f9f9b43d4c4c28cf5400248948d52e5048
dilbert250 <pickertjoe@gmail.com>
2019-10-28 11:17:58 -0500

    Adding name and activity fields to Gpx model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_28_161521) do
+ActiveRecord::Schema.define(version: 2019_10_28_161641) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2019_10_28_161521) do
   create_table "gpxes", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.integer "activity"
   end
 
   create_table "users", force: :cascade do |t|
