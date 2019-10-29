75a31eab0aafa09c1fd8e3236564e5da870459db
dilbert250 <pickertjoe@gmail.com>
2019-10-29 13:35:06 -0500

    Testing user show view

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     resources :gpxes do
       resources :elevationgraphs
       resources :topographicmaps
+    end
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
