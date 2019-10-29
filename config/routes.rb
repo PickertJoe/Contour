d62f0e8c5249beab4796ee070d5facadc5cbce10
dilbert250 <pickertjoe@gmail.com>
2019-10-29 13:28:02 -0500

    Creating show view for user profile; setting up basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
   get 'pages/faq'
+
+  devise_for :users
+  resources :users, shallow:true do
+    resources :gpxes do
+      resources :elevationgraphs
+      resources :topographicmaps
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
