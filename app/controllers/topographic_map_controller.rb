82b8ca06b2f218f53ca0d27d48b939ad057fba20
dilbert250 <pickertjoe@gmail.com>
2019-11-22 10:32:55 -0600

    Adding show, edit, destroy, update, and helper methods to topo controller

--- app/controllers/topographic_map_controller.rb
+++ app/controllers/topographic_map_controller.rb
@@ -22,4 +22,39 @@ class TopographicMapController < ApplicationController
       end
     end
   end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @topographic_map.update(topographic_params)
+        format.html { redirect_to @topographic_map }
+      else
+        flash.now[:alert] = "Could not create topographic map. Please check input."
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @topographic_map.destroy
+    redirect_to gpx_topographic_maps_path(@topographic_map.gpx_id), notice: "Your topographic map has been successfully deleted."
+  end
+
+  private
+    def topographic_params
+      params.require(:topographic_map).permit(:chart_title, :gpx_id, :size, :data)
+    end
+
+    def set_gpx
+      @gpx = Gpx.find(params[:gpx_id])
+    end
+
+    def set_topographic
+      @topographic_map = TopographicMap.find(params[:id])
+    end
 end
