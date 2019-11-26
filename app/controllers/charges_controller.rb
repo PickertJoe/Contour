20931597a007043beb8a33331f798bafe543a442
dilbert250 <pickertjoe@gmail.com>
2019-11-26 11:23:04 -0600

    Adding new, create, and supporting methods to charges controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,2 +1,23 @@
 class ChargesController < ApplicationController
+
+  rescue_from Stripe::CardError, with: :catch_exception
+
+  def new
+  end
+
+  def create
+    StripeChaargesServices.new(charges_params, current_user).call
+    redirect_to new_charges_path
+  end
+
+  private
+
+  def charges_params
+    params.permit(:stripeEmail, :stripeToken, :order_id)
+  end
+
+  def catch_exception(exception)
+    flash[:error] = exception.message
+  end
+
 end
