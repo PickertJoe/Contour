2573b5cf0067c7821e8bc3553a9bea368dda6169
dilbert250 <pickertjoe@gmail.com>
2019-11-26 14:08:40 -0600

    Experimenting with stripe checkout integration

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -21,6 +21,6 @@ class ChargesController < ApplicationController
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
-    redirect_to new_charge_path
+    redirect_to root_path
   end
 end
