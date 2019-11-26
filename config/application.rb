945911d0d9d3a2cd5749f8d8246a89b9c8b8d461
dilbert250 <pickertjoe@gmail.com>
2019-11-26 11:43:26 -0600

    Moving Stripe keys to environmental variables

--- config/application.rb
+++ config/application.rb
@@ -13,9 +13,6 @@ module Contour
 
     config.mapbox_token = "pk.eyJ1IjoiZGlsYmVydDI1MCIsImEiOiJjazE2eG41N2cxMjVhM2NsYnFtYXBucWM0In0.qjQFUix7bPaSwLZ8IskknA"
 
-    config.stripe_secret_key: "sk_test_ZMVmBa1aORP4j0nY2grutQhT005Q5ax812"
-    config.stripe_publishable_key: "pk_test_EkW4EWXKkupboL1FEEYYWeXy00rG8oRyEN"
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
