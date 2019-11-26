bf07ba20a7e7e5a3f72feabfa86549146f7f569a
dilbert250 <pickertjoe@gmail.com>
2019-11-26 11:37:11 -0600

    Refactoring charges controller, adding service class for Stripe

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,23 +1,26 @@
 class ChargesController < ApplicationController
 
-  rescue_from Stripe::CardError, with: :catch_exception
-
   def new
   end
 
   def create
-    StripeChaargesServices.new(charges_params, current_user).call
-    redirect_to new_charges_path
+    # Amount in cents
+    @amount = 500
+
+    customer = Stripe::Customer.create({
+      email: params[:stripeEmail],
+      source: params[:stripeToken],
+    })
+
+    charge = Stripe::Charge.create({
+      customer: customer.id,
+      amount: @amount,
+      description: "Rails Stripe customer",
+      currency: "usd",
+    })
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
   end
-
-  private
-
-  def charges_params
-    params.permit(:stripeEmail, :stripeToken, :order_id)
-  end
-
-  def catch_exception(exception)
-    flash[:error] = exception.message
-  end
-
 end
