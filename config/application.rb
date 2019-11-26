5d6f97a131ed4b89fcbba98115376e99cfd85c34
dilbert250 <pickertjoe@gmail.com>
2019-11-26 08:34:47 -0600

    Moving Mapbox API key from JS partial to application config

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Contour
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.mapbox_token = "pk.eyJ1IjoiZGlsYmVydDI1MCIsImEiOiJjazE2eG41N2cxMjVhM2NsYnFtYXBucWM0In0.qjQFUix7bPaSwLZ8IskknA"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
